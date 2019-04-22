498c38299aff29f9099ff844bff5b5a24b487547
FANA <ailynrosas2129@gmail.com>
2019-04-22 18:01:38 -0500

    quinto

---
+++ main.js
@@ -0,0 +1,20 @@
+// let espacios= ''
+
+// for( let i = 0; i <=7; i++){
+
+//     for(let j =0; j<=7; j++){
+//         console.log('b')
+//     }
+// }
+
+// // salto de linea  +='\n'
+
+// ------------------------------------
+ for (let i= 1; i<=100; i++){
+     if(i%3 ===0){
+         console.log(i, 'fizz')
+     }else if(i % 5 ===0){
+         console.log(i, 'buzz')
+        if(i %3 ===0 && i %5)
+     }
+ }
